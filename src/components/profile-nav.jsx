9f0f04600ee188e6cdfbfcf23a4f22fd8534fab2
Raghav <raghav.tandon13@gmail.com>
2023-07-03 14:49:02 +0530

    carousel

--- src/components/profile-nav.jsx
+++ src/components/profile-nav.jsx
@@ -6,10 +6,14 @@ import "./profile-nav.css";
 function ProfileNav() {
   return (
     <div className="menu-bar">
-        <button>Online services</button>
-        <button>eBooks</button>
-        <button>News</button>
-        <button>Bhajan</button>
+      <button>Online services</button>
+      <button>eBooks</button>
+      <button>News</button>
+      <button>Bhajan</button>
+      <button>Accomodation</button>
+      <button>Darshan</button>
+      <button>Sr. Citizen</button>
+      <button>Bhajan</button>
     </div>
   );
 }
