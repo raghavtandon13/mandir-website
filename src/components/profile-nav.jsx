27189b48782f419f5b796128b60cf55da4382c2e
Raghav <raghav.tandon13@gmail.com>
2023-07-05 12:10:30 +0530

    nav

--- src/components/profile-nav.jsx
+++ src/components/profile-nav.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Facebook, Twitter, Instagram } from "@mui/icons-material";
+import MenuIcon from "@mui/icons-material/Menu";
 import "./profile-nav.css";
 
 function ProfileNav() {
   return (
     <div className="menu-bar">
-      <button>Online services</button>
-      <button>eBooks</button>
-      <button>News</button>
-      <button>Bhajan</button>
-      <button>Accomodation</button>
-      <button>Darshan</button>
-      <button>Sr. Citizen</button>
-      <button>Bhajan</button>
+      <input type="checkbox" id="check" />
+      <label htmlFor="check" className="checkbtn">
+          {/* <h2>Services</h2> */}
+          <MenuIcon />
+      </label>
+      <div className="menu-items">
+        <button>Online services</button>
+        <button>eBooks</button>
+        <button>News</button>
+        <button>Bhajan</button>
+        <button>Accomodation</button>
+        <button>Darshan</button>
+        <button>Sr. Citizen</button>
+        <button>Bhajan</button>
+      </div>
     </div>
   );
 }
